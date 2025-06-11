fe92db1d8ee238808dc03b0a0e6e604c16dba80a
justme_herza <157363409+herzonly@users.noreply.github.com>
2025-06-11 09:38:31 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,23 +1,25 @@
 // SCRIPT BY HERZA, DO NOT DELETE WM
-
 global.token = "BOT_TOKEN"
 global.ownername = "OWNER_NAME"
-global.ownerid = ["OWNER_ID"]
-global.premid = ["PREMIUM_ID"]
+global.ownerid = "OWNER_ID"
+global.premid = "PREM_ID"
 global.botname = "BOT_NAME"
-global.prefix = /^[/]/
+global.prefix = ["/", ".", "#", "!"]
 global.wib = 7
-
-
-
+global.wait = "Wait for a moment..."
 
 let fs = require('fs');
 let chalk = require('chalk');
+
 const file = require.resolve(__filename);
+
 fs.watchFile(file, () => {
   fs.unwatchFile(file);
   console.log(chalk.redBright(`Update 'config.js'`));
   delete require.cache[file];
+  require(file);
 });
 
 // SCRIPT TIDAK BOLEH DI PERJUAL BELIKAN
+// jangan lupa kirim foto furry berotot gay ke herzfnf@gmail.com udh
+//g tahan soalnya 😂
